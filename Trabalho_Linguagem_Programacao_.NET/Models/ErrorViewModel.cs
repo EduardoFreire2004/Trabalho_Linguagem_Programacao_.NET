f08cfe8433aefbb9975d71458706699d686db231
Eduardo Dias Souza Freire <eduardodiassouzafreire@gmail.com>
2024-09-18 14:25:52 -0300

    Criar

---
+++ Trabalho_Linguagem_Programacao_.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trabalho_Linguagem_Programacao_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
